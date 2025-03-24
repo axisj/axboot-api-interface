3d4a28090b8c249b2731b40d4e361a336aa9a0df
tom <tom@axisj.com>
2025-03-24 14:22:38 +0900

    메뉴 인터페이스 수정

--- dto/Menu.ts
+++ dto/Menu.ts
@@ -1,17 +1,20 @@
 import { DefaultDto } from "../types";
 
 export interface Menu extends DefaultDto {
+  menuUrlValue?: string; //메뉴 url
   menuGroupId?: string; //메뉴 그룹 코드
   multiLang?: Record<string, any>; //multiLang
   iconKncd?: string; //아이콘 형태
   progNm?: string; //프로그램 명
   upprMenuId?: number; //부모 ID
   progId?: string; //프로그램 코드
+  upddeDtm?: string; //수정 일시
   menuId?: number; //ID
   children?: Menu[]; //메뉴 목록
   sortValue?: number; //정렬
   updatedAt?: string; //수정 일시
   updatedBy?: string; //수정자
+  menuUuid?: string; //메뉴 uuid
   updatedByNm?: string; //수정자 명
   menuLvl?: number; //메뉴 레벨
 }
--- dto/MenuGrp.ts
+++ dto/MenuGrp.ts
@@ -3,10 +3,11 @@ import { DefaultDto } from "../types";
 export interface MenuGrp extends DefaultDto {
   updatedAt?: string; //수정 일시
   updatedBy?: string; //수정자
-  useYn?: string; //사용 여부
   multiLang?: Record<string, any>; //multiLang
   iconKncd?: string; //아이콘 형태
   sortValue?: number; //메뉴 그룹 정렬
   menuGroupId?: string; //메뉴 그룹 ID
+  upddeDtm?: string; //수정 일시
   updatedByNm?: string; //수정자 명
+  useYn?: "Y" | "N"; //Y | N
 }

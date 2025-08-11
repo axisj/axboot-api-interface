b10e1093076fb0cc1456451132a20a5ef74e7690
hyoung <wlsh831021@gmail.com>
2025-08-11 13:41:22 +0900

    인터페이스 수정

--- dto/ProgRoleMenuRes.ts
+++ dto/ProgRoleMenuRes.ts
@@ -4,7 +4,7 @@ import { FnTitleInfo, ProgRoleMenu } from "./index";
 export interface ProgRoleMenuRes extends DefaultDto {
   menuList?: ProgRoleMenu[]; //메뉴 목록
   progNm?: string; //프로그램명
-  fnctNmInfoJson?: FnTitleInfo[]; //기능
+  fnTitleInfo?: FnTitleInfo[]; //기능
   progId?: string; //프로그램ID
   roleId?: string; //역할 ID
   fnctAuthorInfoJson?: string[]; //기능 권한정보 JSON
